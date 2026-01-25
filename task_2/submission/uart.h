ba7bf894363c38f68eeeb32ac14eb78ab13f6a9f
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-01-25 21:28:43 +0530

    Create uart.h

---
+++ task_2/submission/uart.h
@@ -0,0 +1,11 @@
+// uart.h (unchanged)
+#ifndef UART_H
+#define UART_H
+
+#include "ch32v00x.h"
+
+void uart_init(uint32_t baudrate);
+void uart_send_char(char ch);
+void uart_send_string(const char* str);
+
+#endif
