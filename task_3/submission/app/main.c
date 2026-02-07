1b4d736a1ade3117019e226f12f26262d5ebcd7a
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-07 20:08:12 +0530

    Update main.c

--- task_3/submission/app/main.c
+++ task_3/submission/app/main.c
@@ -1,1 +1,24 @@
+#include "timer.h"
+#include "uart.h"
+#include "gpio.h"
+
+int main(void)
+{
+    timer_init();
+    uart_init(9600);
+    gpio_led_init();
+
+    uart_print("Task-3 Timer Demo Started\r\n");
+
+    while (1)
+    {
+        gpio_led_toggle();
+
+        uart_print("Timestamp(ms): ");
+        uart_print_uint(timer_get_ms());
+        uart_print("\r\n");
+
+        timer_delay_ms(1000);
+    }
+}
 
