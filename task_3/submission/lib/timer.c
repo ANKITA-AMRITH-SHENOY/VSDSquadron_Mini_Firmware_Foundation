e57ce1a9e05e3f1967e59520d4c041f6841469c6
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-07 20:06:14 +0530

    Update timer.c

--- task_3/submission/lib/timer.c
+++ task_3/submission/lib/timer.c
@@ -1,1 +1,29 @@
+#include "timer.h"
+
+/* Software millisecond timer */
+static volatile uint32_t ms_ticks = 0;
+
+void timer_init(void)
+{
+    ms_ticks = 0;
+}
+
+void timer_delay_ms(uint32_t ms)
+{
+    volatile uint32_t i;
+
+    while (ms--)
+    {
+        for (i = 0; i < 8000; i++)
+        {
+            __asm__("nop");
+        }
+        ms_ticks++;
+    }
+}
+
+uint32_t timer_get_ms(void)
+{
+    return ms_ticks;
+}
 
