a07335a55b004bf25ad8dfcd074d8094ac41a6db
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-07 20:05:10 +0530

    Update gpio.c

--- task_3/submission/lib/gpio.c
+++ task_3/submission/lib/gpio.c
@@ -1,1 +1,17 @@
+#include "gpio.h"
+#include "ch32v00x.h"
+
+void gpio_led_init(void)
+{
+    RCC->APB2PCENR |= RCC_APB2Periph_GPIOD;
+
+    /* PD6 output push-pull */
+    GPIOD->CFGLR &= ~(0xF << (6 * 4));
+    GPIOD->CFGLR |=  (0x3 << (6 * 4));
+}
+
+void gpio_led_toggle(void)
+{
+    GPIOD->OUTDR ^= (1 << 6);
+}
 
