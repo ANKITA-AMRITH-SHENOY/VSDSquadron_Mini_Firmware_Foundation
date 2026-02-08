ecb3928ca0d1ae1c10eb3fe759dd5ee4a5d99df2
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-09 01:12:54 +0530

    Update gpio.h

--- task4/submission/lib/gpio.h
+++ task4/submission/lib/gpio.h
@@ -1,1 +1,16 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "ch32v00x.h"
+
+/* External LED on PC6 */
+#define LED_PIN  GPIO_Pin_6
+#define LED_PORT GPIOC
+
+void gpio_init(void);
+void gpio_set_high(uint16_t pin);
+void gpio_set_low(uint16_t pin);
+void gpio_toggle(uint16_t pin);
+
+#endif
 
