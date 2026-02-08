feee676fc0b8b4752bdaf9253590407145a0e988
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-09 01:13:53 +0530

    Update pwm.c

--- task4/submission/lib/pwm.c
+++ task4/submission/lib/pwm.c
@@ -1,1 +1,64 @@
+#include "pwm.h"
+#include "gpio.h"
+
+static volatile uint8_t pwm_duty = 0;
+static volatile uint8_t pwm_counter = 0;
+
+void TIM2_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM2_IRQHandler(void)
+{
+    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+    {
+        pwm_counter++;
+        if (pwm_counter >= 100)
+            pwm_counter = 0;
+
+        /* Software PWM on PC6 */
+        if (pwm_counter < pwm_duty)
+            GPIO_SetBits(LED_PORT, LED_PIN);
+        else
+            GPIO_ResetBits(LED_PORT, LED_PIN);
+
+        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+    }
+}
+
+void pwm_init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+    /* PC6 as output for software PWM */
+    GPIO_InitStructure.GPIO_Pin = LED_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(LED_PORT, &GPIO_InitStructure);
+
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure = {0};
+    NVIC_InitTypeDef NVIC_InitStructure = {0};
+
+    uint16_t prescaler = (SystemCoreClock / 10000000) - 1;
+
+    TIM_TimeBaseStructure.TIM_Period = 99;
+    TIM_TimeBaseStructure.TIM_Prescaler = prescaler;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    TIM_Cmd(TIM2, ENABLE);
+}
+
+void pwm_set_duty(uint8_t duty_percent)
+{
+    pwm_duty = duty_percent;
+}
 
