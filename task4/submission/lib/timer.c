44d85fe011b6603eb1dd632c8b3f582b8a782e78
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-09 01:15:32 +0530

    Update timer.c

--- task4/submission/lib/timer.c
+++ task4/submission/lib/timer.c
@@ -1,1 +1,48 @@
+#include "timer.h"
+#include "ch32v00x.h"
+
+volatile uint32_t tick_count = 0;
+
+void TIM1_UP_IRQHandler(void) __attribute__((interrupt("WCH-Interrupt-fast")));
+void TIM1_UP_IRQHandler(void) {
+    if (TIM_GetITStatus(TIM1, TIM_IT_Update) != RESET) {
+        tick_count++;
+        TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+    }
+}
+
+void timer_init(uint32_t tick_hz) {
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure = {0};
+    NVIC_InitTypeDef NVIC_InitStructure = {0};
+    
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+    
+    uint16_t prescaler = (SystemCoreClock / tick_hz / 1000) - 1;
+    uint16_t period = 999;
+    
+    TIM_TimeBaseStructure.TIM_Period = period;
+    TIM_TimeBaseStructure.TIM_Prescaler = prescaler;
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+    
+    TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
+    
+    NVIC_InitStructure.NVIC_IRQChannel = TIM1_UP_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+    
+    TIM_Cmd(TIM1, ENABLE);
+}
+
+void timer_delay_ms(uint32_t ms) {
+    uint32_t start = tick_count;
+    while(tick_count - start < ms);
+}
+
+uint32_t timer_get_tick(void) {
+    return tick_count;
+}
 
