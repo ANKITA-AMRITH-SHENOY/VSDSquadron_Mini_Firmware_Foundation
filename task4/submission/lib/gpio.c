b892f035257e2d5aef1c06b2c27171d3a5085d07
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-09 01:12:33 +0530

    Update gpio.c

--- task4/submission/lib/gpio.c
+++ task4/submission/lib/gpio.c
@@ -1,1 +1,34 @@
+#include "gpio.h"
+
+void gpio_init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure = {0};
+
+    /* Enable GPIOC clock */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+    /* PC6 as output */
+    GPIO_InitStructure.GPIO_Pin = LED_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(LED_PORT, &GPIO_InitStructure);
+}
+
+void gpio_set_high(uint16_t pin)
+{
+    GPIO_SetBits(LED_PORT, pin);
+}
+
+void gpio_set_low(uint16_t pin)
+{
+    GPIO_ResetBits(LED_PORT, pin);
+}
+
+void gpio_toggle(uint16_t pin)
+{
+    if (GPIO_ReadOutputDataBit(LED_PORT, pin))
+        GPIO_ResetBits(LED_PORT, pin);
+    else
+        GPIO_SetBits(LED_PORT, pin);
+}
 
