536daa44305b1ade5cd5fb831122280fb95adfc6
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-09 01:16:58 +0530

    Update uart.h

--- task4/submission/lib/uart.h
+++ task4/submission/lib/uart.h
@@ -1,1 +1,17 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdint.h>
+
+void uart_init(uint32_t baud);
+void uart_send_char(char c);
+void uart_send_string(const char *s);
+
+/* Blocking receive */
+char uart_receive_char(void);
+
+/* Non-blocking check */
+uint8_t uart_char_available(void);
+
+#endif
 
