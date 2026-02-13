3b7a9fbfa475d70d0c3fc9268babc7c167f0eeda
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-14 00:11:31 +0530

    Update gpio.c

--- task_2/submission/gpio.c
+++ task_2/submission/gpio.c
@@ -1,6 +1,6 @@
 #include "gpio.h"
 
-void gpio_init(void)
+void led_init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
@@ -11,15 +11,15 @@ void gpio_init(void)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    GPIO_SetBits(GPIOD, (1 << LED_PIN)); // LED OFF (active-low)
+    GPIO_SetBits(GPIOD, (1 << LED_PIN));  // Start with LED OFF
 }
 
-void gpio_set(int pin)
+void led_on(void)
 {
-    GPIO_SetBits(GPIOD, (1 << pin));   // LED OFF
+    GPIO_SetBits(GPIOD, (1 << LED_PIN));
 }
 
-void gpio_clear(int pin)
+void led_off(void)
 {
-    GPIO_ResetBits(GPIOD, (1 << pin)); // LED ON
+    GPIO_ResetBits(GPIOD, (1 << LED_PIN));
 }
