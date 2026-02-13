60849cd914164393236ccd1533b7f66d2741bcef
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-14 00:12:53 +0530

    Update uart.c

--- task_2/submission/uart.c
+++ task_2/submission/uart.c
@@ -7,7 +7,7 @@ void uart_init(uint32_t baudrate)
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
 
-    // PD5 → TX only
+    // PD5 → TX
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
