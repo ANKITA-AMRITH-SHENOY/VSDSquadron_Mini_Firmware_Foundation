1c6e03f3e126b84956a1fba0f2749ef8939ce586
ANKITA AMRITH SHENOY <ankitaamrithshenoy759@gmail.com>
2026-02-13 21:38:54 +0530

    Update gpio.h

--- task_2/submission/gpio.h
+++ task_2/submission/gpio.h
@@ -3,11 +3,11 @@
 
 #include "ch32v00x.h"
 
-#define LED_PIN 6 // PD6 for VSDSquadron Mini (pin number only)
+#define LED_PIN 6   // PD6
 
 void gpio_init(void);
 void gpio_set(int pin);
 void gpio_clear(int pin);
-void gpio_toggle(int pin);
 
 #endif
+
